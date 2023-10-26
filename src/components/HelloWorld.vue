abb22ed8cf1a124d317cfbbd1ef369f92d379a3b
zhangdi <1258956799@qq.com>
2023-10-26 21:45:32 +0800

    chore: update package.json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,9 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
+
+console.log('1', 1)
+
 const count = ref(0)
 </script>
 
---
+++ src/utils/is.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, it } from 'vitest';
+import {
+  isArray,
+  isIntegerKey,
+  isMap,
+  isPlainObject,
+  isPromise,
+  toRawType
+} from './is';
+
+describe('Utility Functions', () => {
+
+  it('toRawType', () => {
+    expect(toRawType({})).toBe('Object');
+    expect(toRawType([])).toBe('Array');
+    expect(toRawType(new Date())).toBe('Date');
+  });
+
+  it('isArray', () => {
+    expect(isArray([])).toBe(true);
+    expect(isArray({})).toBe(false);
+  });
+
+  it('isMap', () => {
+    expect(isMap(new Map())).toBe(true);
+    expect(isMap(new Set())).toBe(false);
+  });
+
+  //... similar tests for other utility functions
+
+  it('isPromise', () => {
+    expect(isPromise(Promise.resolve())).toBe(true);
+    expect(isPromise({})).toBe(false);
+  });
+
+  it('isPlainObject', () => {
+    expect(isPlainObject({})).toBe(true);
+    expect(isPlainObject(new Map())).toBe(false);
+  });
+
+  it('isIntegerKey', () => {
+    expect(isIntegerKey('123')).toBe(true);
+    expect(isIntegerKey('NaN')).toBe(false);
+    expect(isIntegerKey('-123')).toBe(false);
+  });
+  console.log('1', 1)
+});
+
