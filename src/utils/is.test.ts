f4bf33af0e13669e30dadbda9267adc51f4553c1
zhangdi <1258956799@qq.com>
2023-10-27 14:40:39 +0800

    chore: add Eslint

--- src/utils/is.test.ts
+++ src/utils/is.test.ts
@@ -1,48 +1,45 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
 import {
   isArray,
   isIntegerKey,
   isMap,
   isPlainObject,
   isPromise,
-  toRawType
-} from './is';
+  toRawType,
+} from './is'
 
-describe('Utility Functions', () => {
-
+describe('utility Functions', () => {
   it('toRawType', () => {
-    expect(toRawType({})).toBe('Object');
-    expect(toRawType([])).toBe('Array');
-    expect(toRawType(new Date())).toBe('Date');
-  });
+    expect(toRawType({})).toBe('Object')
+    expect(toRawType([])).toBe('Array')
+    expect(toRawType(new Date())).toBe('Date')
+  })
 
   it('isArray', () => {
-    expect(isArray([])).toBe(true);
-    expect(isArray({})).toBe(false);
-  });
+    expect(isArray([])).toBe(true)
+    expect(isArray({})).toBe(false)
+  })
 
   it('isMap', () => {
-    expect(isMap(new Map())).toBe(true);
-    expect(isMap(new Set())).toBe(false);
-  });
+    expect(isMap(new Map())).toBe(true)
+    expect(isMap(new Set())).toBe(false)
+  })
 
-  //... similar tests for other utility functions
+  // ... similar tests for other utility functions
 
   it('isPromise', () => {
-    expect(isPromise(Promise.resolve())).toBe(true);
-    expect(isPromise({})).toBe(false);
-  });
+    expect(isPromise(Promise.resolve())).toBe(true)
+    expect(isPromise({})).toBe(false)
+  })
 
   it('isPlainObject', () => {
-    expect(isPlainObject({})).toBe(true);
-    expect(isPlainObject(new Map())).toBe(false);
-  });
+    expect(isPlainObject({})).toBe(true)
+    expect(isPlainObject(new Map())).toBe(false)
+  })
 
   it('isIntegerKey', () => {
-    expect(isIntegerKey('123')).toBe(true);
-    expect(isIntegerKey('NaN')).toBe(false);
-    expect(isIntegerKey('-123')).toBe(false);
-  });
-  // console.log('1', 1)
-});
-
+    expect(isIntegerKey('123')).toBe(true)
+    expect(isIntegerKey('NaN')).toBe(false)
+    expect(isIntegerKey('-123')).toBe(false)
+  })
+})
--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,3 +1,4 @@
 export function sum(a:number, b:number) {
+  console.log(1);
   return a + b
 }
